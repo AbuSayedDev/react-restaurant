adb87feaa779f96af93591ac3d3a0a6ae95b8836
sayed285235 <48875366+sayed285235@users.noreply.github.com>
2021-07-13 21:55:20 +0600

    setup project folder

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://abusayed.info"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+            Abu sayed
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
