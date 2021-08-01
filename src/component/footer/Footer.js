237173e49b20acf46c1c4968406a8539b9b4b3ed
sayed285235 <48875366+sayed285235@users.noreply.github.com>
2021-08-01 20:33:15 +0600

    footer integrations

--- src/component/footer/Footer.js
+++ src/component/footer/Footer.js
@@ -4,8 +4,14 @@ import React from"react";
 
 const Footer = () => {
     return(
-        <div>
-
+        <div className="footer text-center">
+            <div className="container">
+                <div className="row align-items-center">
+                    <div className="col-md-12">
+                        <p className="pb-0 mb-0">Copyright 2021 © Restaurant | All rights Reserved. Design by <a href="https://abusayed.info" target="_blank">Abu sayed</a> </p>
+                    </div>
+                </div>
+            </div>
 
         </div>
     );
